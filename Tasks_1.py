71b8329745cb94cfc3fdd15345c8928ba4f0ec52
Wasil-P <it.dzemidok@gmail.com>
2024-02-13 14:00:05 +0100

    Add task 3

--- Tasks_1.py
+++ Tasks_1.py
@@ -60,3 +60,36 @@ def decode(encoded: list[int], first: int) -> list[int]:
 
 print(decode(encoded1, first1))
 print(decode(encoded2, first2))
+
+
+"""You are given an integer n, the number of teams in a tournament that has strange rules:
+
+If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
+If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
+Return the number of matches played in the tournament until a winner is decided.
+
+Input: n = 7
+Output: 6
+Explanation: Details of the tournament: 
+- 1st Round: Teams = 7, Matches = 3, and 4 teams advance.
+- 2nd Round: Teams = 4, Matches = 2, and 2 teams advance.
+- 3rd Round: Teams = 2, Matches = 1, and 1 team is declared the winner.
+Total number of matches = 3 + 2 + 1 = 6."""
+
+n = 7
+n2 = 6
+def numberOfMatches(n: int) -> int:
+
+    list_ = []
+    while n > 1:
+       if n % 2 == 0:
+            list_.append(n // 2)
+            n = n // 2
+       else:
+            n = n - 1
+            list_.append((n // 2))
+            n = ((n // 2) + 1)
+    return sum(list_)
+
+print(numberOfMatches(n))
+print(numberOfMatches(n2))
