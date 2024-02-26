a70786b25fbf18211886afe49130e7d26fdf8aad
Wasil-P <it.dzemidok@gmai.com>
2024-02-26 20:23:17 +0100

    Add task #5

--- Tasks_1.py
+++ Tasks_1.py
@@ -122,3 +122,24 @@ def isPalindrome(x: int) -> bool:
 
 print(isPalindrome(x))
 print(isPalindrome(x2))
+
+
+"""5) You are given a 0-indexed integer array nums of even length and there is also an empty array arr. Alice and Bob decided to play a game where in every round Alice and Bob will do one move. The rules of the game are as follows:
+
+Every round, first Alice will remove the minimum element from nums, and then Bob does the same.
+Now, first Bob will append the removed element in the array arr, and then Alice does the same.
+The game continues until nums becomes empty.
+Return the resulting array arr. """
+
+
+nums = [5, 4, 2, 3]
+def numberGame(nums):
+    nums = sorted(nums)
+    for ind in range(len(nums)):
+        if ind % 2 != 0:
+            nums.insert(ind - 1, nums[ind])
+            nums.pop(ind + 1)
+    return nums
+
+
+print(numberGame(nums))
